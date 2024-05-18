136331c0c918da67c248133a4ea1dfa6a7a70109
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-05-18 15:21:00 +0200

    add letter stats report

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def main():
     text = get_book_text(book_path)
     count = get_word_count(text)
     filename = os.path.basename(book_path)
+    stat = get_letter_stats(text)
+
     print(f"{filename} contains {count} words")
+    print("Letter stats:")
+    print(stat)
 
 
 def get_book_text(path):
@@ -17,6 +21,19 @@ def get_book_text(path):
 def get_word_count(text: str) -> int:
     words = text.split()
     return len(words)
+
+
+def get_letter_stats(text: str) -> object:
+    stats = {}
+
+    for char in text.lower():
+        if char.isalpha():
+            if char in stats:
+                stats[char] += 1
+            else:
+                stats[char] = 1
+
+    return stats
 
 
 main()
