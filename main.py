8e0bf8ac62bfbfef0e336e5dc74d91799ed4de3e
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-05-18 15:41:04 +0200

    sort and improve report output

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ def main():
     count = get_word_count(text)
     filename = os.path.basename(book_path)
     stat = get_letter_stats(text)
+    stat_list = process_letter_stats(stat)
 
-    print(f"{filename} contains {count} words")
-    print("Letter stats:")
-    print(stat)
+    print("[  === BOOK REPORT ===  ]")
+    print(f"{filename} contains {count} words\n")
+    print("Letter statistics -------")
+
+    render_stats(stat_list)
+    print("[  ======= END =======  ]")
 
 
 def get_book_text(path):
@@ -23,7 +27,7 @@ def get_word_count(text: str) -> int:
     return len(words)
 
 
-def get_letter_stats(text: str) -> object:
+def get_letter_stats(text: str) -> dict:
     stats = {}
 
     for char in text.lower():
@@ -34,6 +38,29 @@ def get_letter_stats(text: str) -> object:
                 stats[char] = 1
 
     return stats
+
+
+def stat_sort_on(obj: dict) -> int:
+    return obj["count"]
+
+
+def process_letter_stats(stats: dict) -> list:
+    stat_list = []
+
+    for letter in stats:
+        stat_list.append({
+            "letter": letter,
+            "count": stats[letter]
+        })
+
+    stat_list.sort(reverse=True, key=stat_sort_on)
+
+    return stat_list
+
+
+def render_stats(stat_list: [dict]):
+    for stat in stat_list:
+        print(f"The '{stat['letter']}' character was found {stat['count']} times")
 
 
 main()
